9aa6d8144fb870abebb5763bd3426e92c25e8271
asavarimuthu <asavarimuthu@gmail.com>
2023-01-25 13:10:34 +0000

    Initial Push

---
+++ app/riskdemo/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'riskdemo/test/integration/FirstJourney',
+		'riskdemo/test/integration/pages/RisksList',
+		'riskdemo/test/integration/pages/RisksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, RisksList, RisksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('riskdemo') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheRisksList: RisksList,
+					onTheRisksObjectPage: RisksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
